76297bb664134853e5ea7f000c33dc78c041ab16
c29r3 <nomail@fdn82f2f3.com>
2023-05-28 18:58:56 +0300

    v0.1.5 Added separate fee variable for O(1)Labs delegations. Supercharged reward for O(1)Labs and Foundation to validator

--- Mongo.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-
-class Mongo:
-    def __init__(self):
-        """Initialize our Mongo setup to store the payouts"""
-        client = MongoClient()
-        client = MongoClient('you-mongo-url')
-        self.collection = client.mina.payouts
